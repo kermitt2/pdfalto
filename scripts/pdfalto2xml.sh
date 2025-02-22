2ac4671c4191afd0576d1717f14e18e541131bc9
Luca Foppiano <luca@foppiano.org>
2025-02-23 07:38:29 +0900

    add scripts for easy batch conversion

---
+++ scripts/pdfalto2xml.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+export PDFALTO_PATH=../
+
+# Function to process PDF files
+process_pdf_files() {
+  local input_dir="$1"
+  local output_dir="$2"
+
+  # Create the output directory if it doesn't exist
+  mkdir -p "$output_dir"
+
+  # Loop through all files and directories in the input directory
+  for entry in "$input_dir"/*; do
+    if [ -d "$entry" ]; then
+      # If the entry is a directory, recursively process it
+      local subdir_name=$(basename "$entry")
+      process_pdf_files "$entry" "$output_dir/$subdir_name"
+    elif [ -f "$entry" ] && [[ "$entry" == *.pdf ]]; then
+      # If the entry is a PDF file, process it with pdfalto
+      echo "Processing $entry"
+      local filename=$(basename "$entry")
+      ${PDFALTO_PATH}/./pdfalto -noImageInline -fullFontName -noImage -readingOrder "$entry" "$output_dir/${filename%.pdf}.xml"
+    fi
+  done
+}
+
+# Main script execution
+input_directory="$1"
+output_directory="$2"
+
+if [ -z "$input_directory" ] || [ -z "$output_directory" ]; then
+  echo "Usage: $0 <input_directory> <output_directory>"
+  exit 1
+fi
+
+process_pdf_files "$input_directory" "$output_directory"
